de8c31f835d2714168f09f06f1e21722efff0f1a
SS47816 <ss1057532022@gmail.com>
2020-11-01 21:17:54 +0800

    feat: done task 1

--- Homework/Homework I/lesson one/pca_normal.py
+++ Homework/Homework I/lesson one/pca_normal.py
@@ -51,14 +51,14 @@ def PCA(data, correlation=False, sort=True):
 
 def main():
     # 指定点云路径
-    # cat_index = 10 # 物体编号，范围是0-39，即对应数据集中40个物体
-    # root_dir = '/Users/renqian/cloud_lesson/ModelNet40/ply_data_points' # 数据集路径
-    # cat = os.listdir(root_dir)
-    # filename = os.path.join(root_dir, cat[cat_index],'train', cat[cat_index]+'_0001.ply') # 默认使用第一个点云
+    cat_index = 1 # 物体编号，范围是0-39，即对应数据集中40个物体
+    root_dir = '../../../ModelNet40' # 数据集路径
+    cat = os.listdir(root_dir)
+    filename = os.path.join(root_dir, cat[cat_index],'train', cat[cat_index]+'_0001.ply') # 默认使用第一个点云
 
     # 加载原始点云
     # point_cloud_pynt = PyntCloud.from_file("/Users/renqian/Downloads/program/cloud_data/11.ply")
-    point_cloud_pynt = PyntCloud.from_file("../ /cloud_data/11.ply")
+    point_cloud_pynt = PyntCloud.from_file("../../../ModelNet40/airplane/train/airplane_0001.ply")
     point_cloud_o3d = point_cloud_pynt.to_instance("open3d", mesh=False)
     # o3d.visualization.draw_geometries([point_cloud_o3d]) # 显示原始点云
 
@@ -71,21 +71,21 @@ def main():
     point_cloud_vector = v[:, 2] #点云主方向对应的向量
     print('the main orientation of this pointcloud is: ', point_cloud_vector)
     # TODO: 此处只显示了点云，还没有显示PCA
-    # o3d.visualization.draw_geometries([point_cloud_o3d])
-    
-    # 循环计算每个点的法向量
-    pcd_tree = o3d.geometry.KDTreeFlann(point_cloud_o3d)
-    normals = []
-    # 作业2
-    # 屏蔽开始
-
-    # 由于最近邻搜索是第二章的内容，所以此处允许直接调用open3d中的函数
-
-    # 屏蔽结束
-    normals = np.array(normals, dtype=np.float64)
-    # TODO: 此处把法向量存放在了normals中
-    point_cloud_o3d.normals = o3d.utility.Vector3dVector(normals)
     o3d.visualization.draw_geometries([point_cloud_o3d])
+    
+    # # 循环计算每个点的法向量
+    # pcd_tree = o3d.geometry.KDTreeFlann(point_cloud_o3d)
+    # normals = []
+    # # 作业2
+    # # 屏蔽开始
+
+    # # 由于最近邻搜索是第二章的内容，所以此处允许直接调用open3d中的函数
+
+    # # 屏蔽结束
+    # normals = np.array(normals, dtype=np.float64)
+    # # TODO: 此处把法向量存放在了normals中
+    # point_cloud_o3d.normals = o3d.utility.Vector3dVector(normals)
+    # o3d.visualization.draw_geometries([point_cloud_o3d])
 
 
 if __name__ == '__main__':
