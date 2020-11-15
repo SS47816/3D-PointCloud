1950b7705ac044e92eeb9dade31aa528b7eebc41
SS47816 <ss1057532022@gmail.com>
2020-11-15 23:29:16 +0800

    feat: add Spectral Clustering

--- Homework/Homework III/src/compare_cluster.py
+++ Homework/Homework III/src/compare_cluster.py
@@ -17,6 +17,7 @@ from itertools import cycle, islice
 
 from KMeans import KMeans
 from GMM import GMM
+from SpectralClustering import SpectralClustering
 
 np.random.seed(0)
 
@@ -105,6 +106,7 @@ for i_dataset, (dataset, algo_params) in enumerate(datasets):
     # 自编的K-Means、GMM算法
     my_kmeans = KMeans(n_clusters=params['n_clusters'])
     my_gmm = GMM(n_clusters=params['n_clusters'])
+    my_spectral = SpectralClustering(n_clusters=params['n_clusters'], eigen_solver='arpack', affinity="nearest_neighbors")
     # sklearn中自带的算法
     ms = cluster.MeanShift(bandwidth=bandwidth, bin_seeding=True)
     two_means = cluster.MiniBatchKMeans(n_clusters=params['n_clusters'])
@@ -130,6 +132,7 @@ for i_dataset, (dataset, algo_params) in enumerate(datasets):
     clustering_algorithms = (
         ('My_KMeans', my_kmeans),
         ('My_GMM', my_gmm),
+        ('My_SpectralClustering', my_spectral),
         ('MiniBatchKMeans', two_means),
         ('AffinityPropagation', affinity_propagation),
         ('MeanShift', ms),
